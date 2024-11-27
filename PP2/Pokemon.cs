43f78a2c36d31959a5f8a8550edfaa31c64d065a
marcushoja0108 <marcushj@live.no>
2024-11-27 14:15:27 +0100

    Add project files.

---
+++ PP2/Pokemon.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP2
+{
+    internal class Pokemon
+    {
+        public string Name;
+        public bool IsHungry;
+        public bool Bured;
+        public bool NeedsToilet;
+
+
+        public Pokemon(string name)
+        {
+            Name = name;
+            IsHungry = true;
+            Bured = true;
+            NeedsToilet = true;
+        }
+
+        public void Needs(int needs)
+        {
+            switch (needs)
+            {
+                case 0:
+                    IsHungry = true;
+                    break;
+                case 1:
+                    Bured = true;
+                    break;
+                case 2:
+                    NeedsToilet = true;
+                    break;
+            }
+        }
+
+        public void Toilet()
+        {
+            if (NeedsToilet)
+
+            {
+                NeedsToilet = false;
+                Console.WriteLine($"{Name} må på do");
+            }
+            else
+            {
+                Console.WriteLine("Må ikke på do");
+
+            }
+        }
+
+        public void Mat()
+        {
+            if (IsHungry)
+            {
+                IsHungry = false;
+                Console.WriteLine($"{Name} trenger mat");
+            }
+            else
+            {
+
+                Console.WriteLine($"{Name} er mett og fornøyd ");
+            }
+
+        }
+
+        public void GiveAttention()
+        {
+            if (Bured)
+            {
+                Bured = false;
+                Console.WriteLine($"{Name} trenger kos");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} trenger ikke kos");
+            }
+        }
+    }
+}
---
+++ PP2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Runtime.InteropServices;
+using PP2;
+
+string pokemonName;
+bool isTrue = true;
+Pokemon selectedPokemon = null;
+
+Pokemon pikachu = new Pokemon("Pikachu");
+Pokemon browzie = new Pokemon($"Browzie");
+List<Pokemon> array = new List<Pokemon>();
+array.Add(pikachu);
+array.Add(browzie);
+
+while (true)
+{
+    Console.WriteLine("1. Se dine pokemon");
+    Console.WriteLine("2. Legg til pokemon");
+    
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.WriteLine("      Dine Pokemon");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Hvilket dyr vil du ta vare på?");
+            pokemonName = Console.ReadLine();
+
+
+            foreach (var pokemon in array)
+            {
+                if (pokemon.Name == pokemonName)
+                {
+                    selectedPokemon = pokemon;
+                }
+            }
+
+            if (selectedPokemon != null)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"1 - Sjekk om {selectedPokemon.Name} må på do ");
+                Console.WriteLine($"2 - Gi {selectedPokemon.Name} mat");
+                Console.WriteLine($"3 - Kos med {selectedPokemon.Name}");
+                Console.WriteLine("4 - Avslutt");
+            }
+            else
+            {
+                Console.WriteLine("Ingen Pokemon med det navnet");
+            }
+
+
+            while (selectedPokemon != null)
+            {
+                Random random = new Random();
+                int needs = random.Next(0, 4);
+                if (needs < 3)
+                {
+                    selectedPokemon.Needs(needs);
+                }
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        selectedPokemon.Toilet();
+
+                        break;
+                    case "2":
+                        selectedPokemon.Mat();
+
+                        break;
+                    case "3":
+                        selectedPokemon.GiveAttention();
+                        break;
+                    case "4":
+                        selectedPokemon = null;
+                        Console.WriteLine("Avslutter");
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Ingen gyldig kommando");
+                        break;
+                }
+            }
+            break;
+        case "2":
+            Console.Write("Pokemon Navn:  ");
+            string name = Console.ReadLine();
+            Pokemon newPokemon = new Pokemon(name);
+            array.Add(newPokemon);
+            break;
+    }
+    
+}
+
